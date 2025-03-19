f1f47573c580b9d2ec886e064a695c62dc2bc8a5
amelnikov <amelnikov@astralinux.ru>
2025-03-19 21:41:16 +0300

    asd

--- set/wsgi.py
+++ set/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'set.settings')
 
 application = get_wsgi_application()
+
+
+def greet():
+    print("Привет из ветки вфвыфф!")
